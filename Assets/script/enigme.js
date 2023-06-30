8a5419cce73f932a8ba7efd6f61087e3c3c9031c
ISAIEL FISTHER BEHAJARIVOLA <priaas823@gmail.com>
2023-06-30 10:45:43 +0200

    Ajouter mes nouveaux fichiers

---
+++ Assets/script/enigme.js
@@ -0,0 +1,170 @@
+// reference overlay backdrop
+const overlay = document.querySelector(".overlay");
+
+// reference formulaire et popup
+const btnConfirm = document.getElementById("btnConfirm");
+const btnPopupQuiz = document.getElementById("btn-popup");
+const textReponse = document.getElementById("reponse");
+const popupTitre = document.querySelector(".popup-titre");
+const popupText = document.querySelector(".popup-text");
+
+// reference validator
+const validator = document.querySelector(".validator");
+
+// compteur question
+const compteurQuiz = document.querySelector(".compteur");
+
+// reference sujet question
+const paraSujet = document.getElementsByClassName("para");
+const aside = document.querySelector(".aside-element");
+
+// creation des element aside
+const asideTitre = document.createElement("h3");
+aside.appendChild(asideTitre);
+const asidePara = document.createElement("p");
+aside.appendChild(asidePara);
+
+// recolte de data dans enigme json
+const urlEnigme = "./json/enigme.json";
+fetch(urlEnigme)
+  .then((reponse) => reponse.json())
+  .then((dataEnigme) => populate(dataEnigme))
+  .catch((err) => {
+    console.err(err);
+  });
+
+let i = 0;
+compteurQuiz.textContent = i + 1;
+
+function populate(dataEnigme) {
+  //----DEFINITION DES FOCTIONS UTILES---------
+  function afficheTrue() {
+    popupTitre.textContent = `BRAVO!!`;
+    popupText.textContent = `Tu as trouvé la réponse.Es tu prêt pour l'énigme suivante ?`;
+    textReponse.value = "";
+    overlay.style.display = "block";
+    btnPopupQuiz.addEventListener("click", function () {
+      overlay.style.display = "none";
+    });
+  }
+
+  function afficheFalse() {
+    popupTitre.textContent = `OUPS!!!`;
+    popupText.textContent = `Vous pouvez encore reessayer`;
+    btnPopupQuiz.textContent = `REESAYER`;
+    textReponse.value = "";
+    overlay.style.display = "block";
+    btnPopupQuiz.addEventListener("click", function () {
+      overlay.style.display = "none";
+    });
+  }
+  //   ---------------------------------------------
+
+  // ----------DEBUT EVENEMENT ----------------------
+  btnConfirm.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    if (textReponse.value === "") {
+      validator.textContent = "Veuillez entrer votre réponse";
+    } else {
+      while (i < dataEnigme.length) {
+        validator.textContent = "";
+        //   transformation des reponses en miniscule
+        const givenReponse = textReponse.value.toLocaleLowerCase();
+        const reponseData = dataEnigme[i].reponse.toLocaleLowerCase();
+
+        if (i === 0) {
+          if (givenReponse === reponseData) {
+            afficheTrue();
+            i++;
+            // effacer les initialisations sur html
+            document.querySelector(".transcrip-aside").remove();
+            document.querySelector(".quote").remove();
+            // partie intro et sujet de enigme
+            paraSujet[0].textContent = dataEnigme[i].intro;
+            paraSujet[1].textContent = dataEnigme[i].sujet;
+            // partie aside
+
+            asideTitre.textContent = `Le savais tu ? `;
+            asidePara.textContent = dataEnigme[i].plus;
+            compteurQuiz.textContent = i + 1;
+
+            // console.log(i);
+          } else {
+            afficheFalse();
+            // console.log(i);
+          }
+        } else if (i === dataEnigme.length - 1) {
+          if (givenReponse === reponseData) {
+            popupTitre.textContent = `TON INITIATION EST TERMINE`;
+            popupText.textContent = ``;
+            btnPopupQuiz.textContent = `REVENIR A L'ECRAN D'ACCUEIL`;
+            textReponse.value = "";
+
+            // ---------PARTIE COMPTE À REBOURS ----------------
+            // definition des dates
+            const dateFuture = new Date("2023-06-30 23:59:59").getTime();
+
+            // definition des references sur html
+            const imgFigma = document.querySelector(".figma img");
+            const minuterText = document.querySelector(".minuter");
+
+            imgFigma.src = "../illustrations+Logo/Enigme/figma_logo.svg";
+
+            let compteurRebours = setInterval(() => {
+              let dateNow = new Date().getTime();
+              let difference = dateFuture - dateNow;
+              // definition des variable jour heure minute seconde
+              let j = Math.floor(difference / (1000 * 60 * 60 * 24));
+              let h = Math.floor(
+                (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+              );
+              let min = Math.floor(
+                (difference % (1000 * 60 * 60)) / (1000 * 60)
+              );
+              let sec = Math.floor((difference % (1000 * 60)) / 1000);
+              // formatage de nombre en unité
+              j = j < 10 ? "0" + j : j;
+              h = h < 10 ? "0" + h : h;
+              min = min < 10 ? "0" + min : min;
+              sec = sec < 10 ? "0" + sec : sec;
+              minuterText.innerHTML = `COMMING SOON... <br><span>${j} : ${h} : ${min} : ${sec}</span>`;
+
+              if (difference < 0) {
+                clearInterval(compteurRebours);
+                minuterText.innerHTML = `C'est l'heure!!`;
+              }
+            }, 1000);
+            // -----------------FIN PARTIE COMPTE A REBOURS---------------
+
+            overlay.style.display = "block";
+            btnPopupQuiz.addEventListener("click", function () {
+              overlay.style.display = "none";
+              window.location.href = "../index.html";
+            });
+            i++;
+            // console.log(i);
+          } else {
+            afficheFalse();
+            // console.log(i);
+          }
+        } else {
+          if (givenReponse === reponseData) {
+            afficheTrue();
+            i++;
+            paraSujet[0].textContent = dataEnigme[i].intro;
+            paraSujet[1].textContent = dataEnigme[i].sujet;
+            asideTitre.textContent = `Le savais tu ? `;
+            asidePara.textContent = dataEnigme[i].plus;
+            compteurQuiz.textContent = i + 1;
+            // console.log(i);
+          } else {
+            afficheFalse();
+            // console.log(i);
+          }
+        }
+
+        break;
+      }
+    }
+  });
+}
---
+++ Assets/script/index-script.js
@@ -0,0 +1,224 @@
+const pantherPlus = document.querySelector("#blackPanther-plus");
+const princeAncre = document.getElementById("prince");
+
+// scroll vers naissance du heros
+pantherPlus.addEventListener("click", function () {
+  princeAncre.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+// carrousel
+// reference de la partie photo
+const photo = document.getElementsByClassName("photo");
+// reference de la partie description
+
+const titre = document.getElementsByClassName("titre");
+const para = document.getElementsByClassName("para");
+
+// animation carrousel
+const conteneurDescr = document.getElementsByClassName("itemTxt");
+
+// bouton suivant
+const btnSuivant = document.querySelector(".chevron");
+
+// recolte de donnee de la carrousel à partir du json
+const urlCarrousel = "./json/carrousel.json";
+fetch(urlCarrousel)
+  .then((result) => result.json())
+  .then((carrousel) => {
+    drawCarrousel(carrousel);
+  })
+  .catch((err) => {
+    console.log(`erreur de chargement de donné ${err}`);
+  });
+// compteur
+let i = 0;
+
+// ---------DEBUT DU PROGRAMME--------------
+// ---------DEFINITION DES FONCTION UTILES--------
+function drawCarrousel(carrousel) {
+  // animation carroussel
+  function animation() {
+    for (let j = 0; j < conteneurDescr.length; j++) {
+      conteneurDescr[j].classList.add("fade");
+      setTimeout(function () {
+        conteneurDescr[j].classList.remove("fade");
+      }, 500);
+    }
+    for (let j = 0; j < photo.length; j++) {
+      photo[j].classList.add("fade");
+      setTimeout(function () {
+        photo[j].classList.remove("fade");
+      }, 500);
+    }
+  }
+  // initialisalisation description
+  // avec animation j+i-1 sans j+i
+  function initialiseDescr(carrousel) {
+    for (let j = 0; j < titre.length; j++) {
+      titre[j].textContent = carrousel[j + i - 1].titre;
+      para[j].textContent = carrousel[j + i - 1].description;
+    }
+  }
+
+  // itnitialisation illustraton
+  // avec animation j+i et sans animation j+i+1
+  function initialiseIllustration() {
+    for (let j = 0; j < photo.length; j++) {
+      photo[j].src = `./illustrations+Logo/Accueil/Pantherhome_slider_${
+        j + i
+      }.png`;
+    }
+  }
+  // initialisation de la carrousel
+  function initialiseCarrousel(carrousel) {
+    // utilisation avec animation
+    setTimeout(() => {
+      initialiseIllustration();
+    }, 400);
+    setTimeout(() => {
+      initialiseDescr(carrousel);
+    }, 400);
+    // utilisation sans animation
+    // initialiseIllustration();
+    // initialiseDescr(carrousel);
+    i++;
+  }
+  // ----------FIN DEFINITIONDES FONCTION UTILE-----------
+
+  initialiseCarrousel(carrousel);
+
+  // -------------DEBUT AVEC L'EVENEMENT CLIC---------------
+
+  btnSuivant.addEventListener("click", function () {
+    if (i < carrousel.length - 2) {
+      // ajout animation avec setTimeOut
+      animation();
+      initialiseCarrousel(carrousel);
+    }
+    // gerer les deux derniers cas particuliers
+    else if (i <= carrousel.length - 2) {
+      const long = carrousel.length;
+      animation();
+      initialiseDescr(carrousel);
+      photo[0].src = `./illustrations+Logo/Accueil/Pantherhome_slider_${
+        i + 1
+      }.png`;
+      photo[1].src = `./illustrations+Logo/Accueil/Pantherhome_slider_${long}.png`;
+      photo[2].src = `./illustrations+Logo/Accueil/Pantherhome_slider_${1}.png`;
+      i++;
+    } else if (i <= carrousel.length - 1) {
+      const long = carrousel.length;
+      animation();
+      photo[0].src = `./illustrations+Logo/Accueil/Pantherhome_slider_${long}.png`;
+      titre[0].textContent = carrousel[i].titre;
+      para[0].textContent = carrousel[i].description;
+      photo[1].src = `./illustrations+Logo/Accueil/Pantherhome_slider_${1}.png`;
+      titre[1].textContent = carrousel[0].titre;
+      para[1].textContent = carrousel[0].description;
+      photo[2].src = `./illustrations+Logo/Accueil/Pantherhome_slider_${2}.png`;
+      i++;
+    } else {
+      i = 0;
+      animation();
+      initialiseCarrousel(carrousel);
+    }
+  });
+}
+// ----------------FIN PARTIE CARROUSEL----------------------
+
+// ---------------PARTIE POPUP----------------------------
+// enregistrement des constiables necessaire à l'evenement
+const btnConfirm = document.getElementById("btnConfirm");
+const btnClose = document.getElementById("btnClose");
+const overlay = document.querySelector(".overlay");
+
+// champ du formulaire et validator
+const validator = document.getElementsByClassName("validator");
+const inputForm = document.getElementsByClassName("inputForm");
+
+// definition de l'evenement ouverture de overlay
+btnConfirm.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  if (
+    inputForm[0].value === "" ||
+    inputForm[1].value === "" ||
+    inputForm[2].value === ""
+  ) {
+    if (inputForm[0].value === "") {
+      validator[0].textContent = `Veuillez enter votre identité`;
+    }
+    if (inputForm[1].value === "") {
+      validator[1].textContent = `Veuillez enter votre adresse email`;
+    }
+    if (inputForm[2].value === "") {
+      validator[2].textContent = `Veuillez enter votre message`;
+    }
+  } else {
+    for (let i = 0; i < inputForm.length; i++) {
+      inputForm[i].value = ``;
+      validator[i].textContent = ``;
+    }
+    ouvrirFenetre();
+  }
+});
+
+function ouvrirFenetre() {
+  overlay.style.display = "block";
+}
+
+btnClose.addEventListener("click", fermerFenetre);
+
+function fermerFenetre() {
+  overlay.style.display = "none";
+}
+
+// --------------FIN PARTIE POPUP------------------------
+
+// DEBUT ANIMATION AVEC LA SOURIS------------------------
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+const img = new Image();
+img.src = "../illustrations+Logo/Accueil/logo_souris_BP2_blanc.png";
+
+let mouseX = 320;
+let mouseY = 80;
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+let pressed = false;
+
+// evenement qui relie l'image au mouvement de la souris
+img.onload = function () {
+  canvas.addEventListener("mousemove", (evt) => {
+    if (!pressed) {
+      mouseX = evt.clientX - this.offsetLeft;
+      mouseY = evt.clientY - this.offsetTop;
+    }
+  });
+
+  // fonction pour animer la l'image
+  function drawImageWithMouse() {
+    ctx.clearRect(0, 0, width, height);
+    ctx.drawImage(img, mouseX - img.width / 2, mouseY - img.height / 2);
+    requestAnimationFrame(drawImageWithMouse);
+  }
+  drawImageWithMouse();
+
+  // desactiver le suivi avec la souris
+  canvas.addEventListener("click", () => {
+    if (!pressed) {
+      cancelAnimationFrame(drawImageWithMouse);
+      mouseX = width / 3.3;
+      mouseY = height / 2.7;
+      pressed = true;
+    }
+  });
+
+  // reactiver le logo avec la souris
+  canvas.addEventListener("mouseout", function () {
+    requestAnimationFrame(drawImageWithMouse);
+    pressed = false;
+  });
+};
